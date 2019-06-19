f842e311ffc76d6b6bd4eaa8b42182ec40c53235
makehoney <makehoney3@gmail.com>
2019-06-19 18:51:56 +0900

    [Programmers/stack_queue] h 인덱스 (not resolved)

--- programmers/sort/src/h_index/Solution.java
+++ programmers/sort/src/h_index/Solution.java
@@ -1,4 +1,48 @@
 package h_index;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/* not resolved */
+
 public class Solution {
+    // 문제를 잘못이해했나?
+    public int solution(int[] citations) {
+        int answer = 0;
+        int numOfEssay = citations.length;
+
+        List<Integer> cits = Arrays.stream(citations).boxed()
+                .sorted((c1, c2) -> c2 - c1)
+                .collect(Collectors.toList());
+
+        for (Integer cit : cits) {
+            int restChance = numOfEssay - cit + 1;
+            int satisfiedIdx = 0;
+            for (Integer comparedCit: cits) {
+                if (restChance <= 0) {
+                    satisfiedIdx = 0;
+                    break;
+                }
+
+                if (satisfiedIdx >= cit) break;
+
+                if (comparedCit >= cit) satisfiedIdx++;
+                else restChance--;
+            }
+            if (satisfiedIdx >= cit) {
+                answer = satisfiedIdx;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] citations = {1, 2, 3, 4 ,5, 6, 7, 8};
+        int result = sol.solution(citations);
+        System.out.println(result);
+    }
 }
--- programmers/sort/src/h_index/Solution_1.java
+++ programmers/sort/src/h_index/Solution_1.java
@@ -1,4 +1,61 @@
+
 package h_index;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+/* not resolved */
+
 public class Solution_1 {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int numOfEssay = citations.length;
+        List<Integer> satisfiedElement = new ArrayList<>();
+        List<Integer> cits = Arrays.stream(citations).boxed()
+                .sorted((c1, c2) -> c2 - c1)
+                .collect(Collectors.toList());
+
+        for (Integer cit : cits) {
+            boolean flag = true;
+            boolean restCondition = true;
+            int restChance = numOfEssay - cit + 1;
+            int counter = 0;
+            int satisfiedIdx = 0;
+
+            for (Integer comparedCit: cits) {
+                if (restChance <= 0) break;
+
+                if (counter >= cit && flag) {
+                    satisfiedIdx = counter;
+                    flag = false;
+                }
+
+                if (comparedCit >= cit) {
+                    satisfiedElement.add(comparedCit);
+                    counter++;
+                } else {
+                    restChance--;
+                }
+            }
+            if (satisfiedElement.size() > satisfiedIdx) {
+                int max = Collections.max(satisfiedElement);
+                if (max > cit) restCondition = false;
+            }
+            satisfiedElement.clear();
+
+            if (satisfiedIdx >= cit && restCondition) {
+                answer = satisfiedIdx;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] citations = {1, 2, 3, 4, 5, 6, 7, 8};
+        int result = sol.solution(citations);
+        System.out.println(result);
+    }
 }
